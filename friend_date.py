da3d580ecb8bee2fb5c5be6c04462f51828bfac5
Nathen2272 <119437741+Nathen2272@users.noreply.github.com>
2023-05-05 10:09:49 -0400

    Add files via upload

---
+++ friend_date.py
@@ -0,0 +1,11 @@
+def friend_date(a, b):
+   
+
+        >>> elmo = ('Elmo', 5, ['hugging', 'being nice'])
+        >>> sauron = ('Sauron', 5000, ['killing hobbits', 'chess'])
+        >>> gandalf = ('Gandalf', 10000, ['waving wands', 'chess'])
+
+        >>> friend_date(elmo, sauron)
+        False
+
+        >>> friend_date(sauron, gandalf)
---
+++ product.py
@@ -0,0 +1,8 @@
+def product(a, b):
+    
+        >>> product(2, 2)
+        4
+
+        >>> product(2, -2)
+        -4
+    return a * b
